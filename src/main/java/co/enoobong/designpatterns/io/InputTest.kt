607df08a27890cbc5f458707914e2590be4cae05
Ibanga Enoobong Ime <eno.ibanga@crowninteractive.com>
2017-08-16 10:54:08 +0100

    feat: IO decorator complete

--- src/main/java/co/enoobong/designpatterns/io/InputTest.kt
+++ src/main/java/co/enoobong/designpatterns/io/InputTest.kt
@@ -10,31 +10,17 @@ import java.io.IOException
  */
 object InputTest {
 
-    @JvmStatic fun main(args: Array<String>){
+    @JvmStatic
+    fun main(args: Array<String>) {
+
         try {
             val inputStream = LowerCaseInputStream(BufferedInputStream(FileInputStream("test.txt")))
 
-            for (c in inputStream.lines){
-                print(c.toChar())
-            }
-        } catch (e: IOException){
+            inputStream.reader().forEachLine { print(it) }
+        } catch (e: IOException) {
             e.printStackTrace()
         }
     }
 
 
 }
-
-val LowerCaseInputStream.lines: Iterator<Int>
-    get() = object : Iterator<Int> {
-        var line = this@lines.read()
-        override fun next(): Int {
-            if (line == -1)
-                throw NoSuchElementException()
-            val result = line
-            line = this@lines.read()
-            return result
-        }
-
-        override fun hasNext() = line != -1
-    }
